daf14eeccd7dcf44755a98667527d423ba867a12
Student <Student@LabVM2047644.fdgattovmioube442nzl3vacha.jnbx.internal.cloudapp.net>
2026-04-01 10:15:31 +0200

    my first commit

---
+++ src/main/java/com/mycompany/gitexample/GitExample.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gitexample;
+
+/**
+ *
+ * @author Student
+ */
+public class GitExample {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
